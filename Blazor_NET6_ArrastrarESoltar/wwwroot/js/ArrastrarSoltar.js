e54d236e517048fb52af26e730ad9537fabed613
laxesta <saracyan78@hotmail.com>
2022-05-27 23:04:58 +0200

    Add project files.

---
+++ Blazor_NET6_ArrastrarESoltar/wwwroot/js/ArrastrarSoltar.js
@@ -0,0 +1,36 @@
+﻿function arrastrarSoltar(className) {
+    const position = { x: 0, y: 0 }
+
+    interact(className).draggable({
+        listeners: {
+            start(event) {
+                console.log(event.type, event.target)
+            },
+            move(event) {
+                position.x += event.dx
+                position.y += event.dy
+
+                event.target.style.transform =
+                    `translate(${position.x}px, ${position.y}px)`
+            },
+        }, modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: '.main',
+                endOnly: true
+            })]
+    })
+};
+
+function zonaEntrega(obxetivoEntrega) {
+    interact(obxetivoEntrega)
+        .dropzone({
+            ondrop: function (event) {
+                alert(event.relatedTarget.id
+                    + ' queda situado en '
+                    + event.target.id)
+            }
+        })
+        .on('dropactivate', function (event) {
+            event.target.classList.add('drop-activated')
+        })
+};
